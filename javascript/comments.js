daed32792fe4ac3a15b1fd973558a10e23fa1617
Abdhul Basith <116423722+abd456@users.noreply.github.com>
2023-08-04 19:06:58 +0530

    Add files via upload

---
+++ javascript/comments.js
@@ -0,0 +1,22 @@
+
+function validateForm() {
+    let x = document.forms["myForm"]["feedback"].value;
+        if (x == "") {
+            document.getElementById("notFilled").innerHTML= 
+            "<div class='questionBox' style ='background-color: red; color: white;' >&#9888; This question is required</div>";
+            
+            document.getElementById("feedbackId").style.border="solid 1px red";
+
+            
+            return false;
+        }
+        
+        thankYouMessage();             
+    }
+    
+function thankYouMessage() {
+    document.getElementById("myForm").style.display = "none";
+    document.getElementById("ty").style.display="block";
+    
+    document.getElementById("footer").style.display="none";
+}
